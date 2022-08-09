8018320c730430b39feb200255131e63bd4e9f54
DevDrizzy <fistergaming123@gmail.com>
2022-08-09 12:53:22 +0500

    Forgot to code this 💀

--- src/main/java/xyz/refinedev/api/storage/impl/BasicYamlStorage.java
+++ src/main/java/xyz/refinedev/api/storage/impl/BasicYamlStorage.java
@@ -1,5 +1,13 @@
 package xyz.refinedev.api.storage.impl;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import xyz.refinedev.api.storage.YamlStorage;
+import xyz.refinedev.api.storage.utils.ConfigValue;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * This Project is property of Refine Development © 2021 - 2022
  * Redistribution of this Project is not allowed
@@ -7,7 +15,49 @@ package xyz.refinedev.api.storage.impl;
  * @author Drizzy
  * Created: 7/27/2022
  * Project: StorageAPI
- */
 
-public class BasicYamlStorage {
+ * This class is basically what default bukkit config is.
+ * It saves the pre-defined file from your plugin and uses that.
+ * No pre-defined config fields or comments.
+ *
+ * This can be used for language config, menu config etc.
+ */
+public class BasicYamlStorage extends YamlStorage {
+
+    /**
+     * Initiation method for a config file
+     *
+     * @param plugin       {@link JavaPlugin plugin instance}
+     * @param name         {@link String config file name}
+     */
+    public BasicYamlStorage(JavaPlugin plugin, String name) {
+        super(plugin, name, true);
+    }
+
+    /**
+     * Comments that are not by config values but added
+     * in paths that are separate.
+     */
+    public void addSeparateComments() {
+        // Nothing
+    }
+
+    /**
+     * Returns the configuration fields with {@link ConfigValue}
+     * annotation on them.
+     *
+     * @return {@link List}
+     */
+    public List<Field> getConfigFields() {
+        return Collections.emptyList(); // No pre-defined stuff
+    }
+
+    /**
+     * The header for this configuration file
+     *
+     * @return {@link String[]} header
+     */
+    public String[] getHeader() {
+        return new String[]{"This configuration file is part of a Refine Development Project. Purchased at https://dsc.gg/refine"};
+    }
 }
