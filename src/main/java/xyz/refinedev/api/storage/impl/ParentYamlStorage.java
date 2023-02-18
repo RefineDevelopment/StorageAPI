3c20ceb740162d4a880e21048341dd91bd93caf0
DevDrizzy <fistergaming123@gmail.com>
2023-02-18 19:48:23 +0500

    Fix null pointers
    Signed-off-by: DevDrizzy <fistergaming123@gmail.com>

--- src/main/java/xyz/refinedev/api/storage/impl/ParentYamlStorage.java
+++ src/main/java/xyz/refinedev/api/storage/impl/ParentYamlStorage.java
@@ -1,6 +1,9 @@
 package xyz.refinedev.api.storage.impl;
 
+import com.google.common.base.Preconditions;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
 import xyz.refinedev.api.storage.YamlStorage;
 import xyz.refinedev.api.storage.annotations.ConfigValue;
 
@@ -13,7 +16,7 @@ public abstract class ParentYamlStorage extends YamlStorage {
      * Set based cache for child storages of this Parent Storage
      * We keep it concurrent to allow asynchronous file I/O
      */
-    private final Set<ChildYamlStorage> childStorages = Collections.synchronizedSet(new HashSet<>());
+    private Set<ChildYamlStorage> childStorages;
 
     /**
      * Initiation method for a config file
@@ -31,6 +34,13 @@ public abstract class ParentYamlStorage extends YamlStorage {
      * @param storage {@link ChildYamlStorage storage}
      */
     public void addChildStorage(ChildYamlStorage storage) {
+        Preconditions.checkNotNull(storage, "[StorageAPI] Child Storage can not be null!");
+
+        // Bypass for constructor being called before default variable initialization
+        if (childStorages == null) {
+            this.childStorages = Collections.synchronizedSet(new HashSet<>());
+        }
+
         this.childStorages.add(storage);
     }
 
@@ -45,6 +55,9 @@ public abstract class ParentYamlStorage extends YamlStorage {
 
         // Add child fields
         this.childStorages.stream().map(ChildYamlStorage::getConfigFields).forEach(annotatedFields::addAll);
+
+        Preconditions.checkArgument(annotatedFields.stream().allMatch(field -> field.isAnnotationPresent(ConfigValue.class)),
+                "[Storage-API] One of your field is missing annotation!");
 
         // Sort according to priority
         annotatedFields.sort(Comparator.comparingInt(field -> field.getAnnotation(ConfigValue.class).priority()));
